a4f465cd1a3d107596b011f2724fe049e32c05d4
weclubasia <110809954+weclubasia@users.noreply.github.com>
2022-08-08 15:37:20 +0800

    Add files via upload

---
+++ script/popvideo/pop-video.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  
+  $('.popup-btn').on('click', function(){
+    $('.video-popup').fadeIn('slow');
+    return false;
+  });
+  
+  $('.popup-bg').on('click', function(){
+    $('.video-popup').slideUp('slow');
+    return false;
+  });
+  
+   $('.close-btn').on('click', function(){
+     $('.video-popup').fadeOut('slow');
+      return false;
+   });
+  
+});
---
+++ script/slider/initslider.js
@@ -0,0 +1,357 @@
+jQuery(document).ready(function(){
+
+    var jsFolder = "";
+
+    jQuery("#amazingslider-12").amazingslider({
+
+        jsfolder:jsFolder,
+
+        width:600,
+
+        height:300,
+
+        loadimageondemand:false,
+
+        enabletouchswipe:true,
+
+        fullscreen:false,
+
+        autoplayvideo:false,
+
+        addmargin:false,
+
+        randomplay:false,
+
+        isresponsive:true,
+
+        pauseonmouseover:false,
+
+        playvideoonclickthumb:true,
+
+        slideinterval:5000,
+
+        fullwidth:false,
+
+        transitiononfirstslide:false,
+
+        scalemode:"fill",
+
+        loop:0,
+
+        autoplay:true,
+
+        navplayvideoimage:"play-32-32-0.png",
+
+        navpreviewheight:60,
+
+        timerheight:2,
+
+        descriptioncssresponsive:"font-size:12px;",
+
+        shownumbering:false,
+
+        skin:"Events",
+
+        textautohide:true,
+
+        addgooglefonts:true,
+
+        navshowplaypause:true,
+
+        navshowplayvideo:true,
+
+        navshowplaypausestandalonemarginx:8,
+
+        navshowplaypausestandalonemarginy:8,
+
+        navbuttonradius:0,
+
+        navthumbnavigationarrowimageheight:32,
+
+        navradius:0,
+
+        lightboxshownavigation:false,
+
+        showshadow:false,
+
+        navfeaturedarrowimagewidth:16,
+
+        navpreviewwidth:120,
+
+        googlefonts:"Inder",
+
+        textpositionmarginright:24,
+
+        bordercolor:"#ffffff",
+
+        lightboxdescriptionbottomcss:"{color:#333; font-size:12px; font-family:Arial,Helvetica,sans-serif; overflow:hidden; text-align:left; margin:4px 0px 0px; padding: 0px;}",
+
+        lightboxthumbwidth:80,
+
+        navthumbnavigationarrowimagewidth:32,
+
+        navthumbtitlehovercss:"text-decoration:underline;",
+
+        texteffectresponsivesize:600,
+
+        arrowwidth:32,
+
+        texteffecteasing:"easeOutCubic",
+
+        texteffect:"slide",
+
+        lightboxthumbheight:60,
+
+        navspacing:8,
+
+        playvideoimage:"playvideo-64-64-0.png",
+
+        ribbonimage:"ribbon_topleft-0.png",
+
+        navwidth:120,
+
+        navheight:60,
+
+        arrowimage:"arrows-32-32-0.png",
+
+        timeropacity:0.6,
+
+        titlecssresponsive:"font-size:12px;",
+
+        navthumbnavigationarrowimage:"carouselarrows-32-32-0.png",
+
+        navshowplaypausestandalone:false,
+
+        texteffect1:"slide",
+
+        navpreviewbordercolor:"#ffffff",
+
+        ribbonposition:"topleft",
+
+        navthumbdescriptioncss:"display:block;position:relative;padding:2px 4px;text-align:left;font:normal 12px Arial,Helvetica,sans-serif;color:#333;",
+
+        lightboxtitlebottomcss:"{color:#333; font-size:14px; font-family:Armata,sans-serif,Arial; overflow:hidden; text-align:left;}",
+
+        arrowstyle:"mouseover",
+
+        navthumbtitleheight:18,
+
+        textpositionmargintop:24,
+
+        navswitchonmouseover:false,
+
+        navarrowimage:"navarrows-28-28-0.png",
+
+        arrowtop:50,
+
+        textstyle:"static",
+
+        playvideoimageheight:64,
+
+        navfonthighlightcolor:"#666666",
+
+        showbackgroundimage:false,
+
+        navpreviewborder:4,
+
+        navshowplaypausestandaloneheight:28,
+
+        shadowcolor:"#aaaaaa",
+
+        navbuttonshowbgimage:true,
+
+        navbuttonbgimage:"navbuttonbgimage-28-28-0.png",
+
+        textbgcss:"display:block; position:absolute; top:0px; left:0px; width:100%; height:100%; background-color:#333333; opacity:0.6; filter:alpha(opacity=60);",
+
+        textpositiondynamic:"bottomleft",
+
+        navpreviewarrowwidth:16,
+
+        navdirection:"horizontal",
+
+        backgroundimagewidth:120,
+
+        navshowpreviewontouch:false,
+
+        bottomshadowimagewidth:110,
+
+        showtimer:true,
+
+        navmultirows:false,
+
+        playvideoimagewidth:64,
+
+        navshowpreview:false,
+
+        navmarginy:16,
+
+        navmarginx:16,
+
+        navfeaturedarrowimage:"featuredarrow-16-8-0.png",
+
+        showribbon:false,
+
+        navstyle:"thumbnails",
+
+        textpositionmarginleft:24,
+
+        descriptioncss:"display:block; position:relative; margin-top:4px; font:12px Inder,Arial,Tahoma,Helvetica,sans-serif; color:#fff;",
+
+        navplaypauseimage:"navplaypause-28-28-0.png",
+
+        backgroundimagetop:-10,
+
+        timercolor:"#ffffff",
+
+        numberingformat:"%NUM/%TOTAL ",
+
+        navfontsize:12,
+
+        navhighlightcolor:"#333333",
+
+        texteffectdelay1:1000,
+
+        navimage:"bullet-24-24-5.png",
+
+        texteffectduration1:600,
+
+        navshowplaypausestandaloneautohide:false,
+
+        navbuttoncolor:"#999999",
+
+        navshowarrow:true,
+
+        texteffectslidedirection:"left",
+
+        navshowfeaturedarrow:true,
+
+        lightboxbarheight:64,
+
+        titlecss:"display:block; position:relative; font: 14px Inder,Arial,Tahoma,Helvetica,sans-serif; color:#fff;",
+
+        ribbonimagey:0,
+
+        ribbonimagex:0,
+
+        texteffectslidedistance1:120,
+
+        navrowspacing:8,
+
+        navshowplaypausestandaloneposition:"bottomright",
+
+        navshowbuttons:false,
+
+        lightboxthumbtopmargin:12,
+
+        arrowhideonmouseleave:1000,
+
+        navshowplaypausestandalonewidth:28,
+
+        navfeaturedarrowimageheight:8,
+
+        navopacity:0.8,
+
+        navthumbtitlecss:"display:block;position:relative;padding:2px 4px;text-align:center;font:bold 12px Arial,Helvetica,sans-serif;color:#333;",
+
+        navcolor:"#999999",
+
+        navthumbtitlewidth:120,
+
+        navpreviewposition:"top",
+
+        texteffectseparate:false,
+
+        arrowheight:32,
+
+        arrowmargin:8,
+
+        texteffectduration:1000,
+
+        bottomshadowimage:"bottomshadow-110-95-4.png",
+
+        border:6,
+
+        lightboxshowdescription:false,
+
+        timerposition:"bottom",
+
+        navfontcolor:"#333333",
+
+        navthumbnavigationstyle:"arrow",
+
+        borderradius:0,
+
+        navbuttonhighlightcolor:"#333333",
+
+        textpositionstatic:"bottom",
+
+        navthumbstyle:"imageandtitle",
+
+        texteffecteasing1:"easeOutCubic",
+
+        textcss:"display:block; padding:12px; text-align:left;",
+
+        navbordercolor:"#ffffff",
+
+        navpreviewarrowheight:8,
+
+        navpreviewarrowimage:"previewarrow-16-8-0.png",
+
+        showbottomshadow:true,
+
+        texteffectslidedistance:30,
+
+        texteffectdelay:500,
+
+        textpositionmarginstatic:0,
+
+        backgroundimage:"",
+
+        navposition:"bottom",
+
+        texteffectslidedirection1:"right",
+
+        navborder:2,
+
+        textformat:"",
+
+        bottomshadowimagetop:95,
+
+        texteffectresponsive:true,
+
+        shadowsize:5,
+
+        lightboxthumbbottommargin:8,
+
+        textpositionmarginbottom:24,
+
+        ribbonmarginy:0,
+
+        ribbonmarginx:0,
+
+        lightboxshowtitle:true,
+
+        slide: {
+
+            duration:1000,
+
+            easing:"easeOutCubic",
+
+            checked:true
+
+        },
+
+        transition:"slide",
+
+        isfullscreen:false,
+
+        textformat: {
+
+
+
+        }
+
+    });
+
+});
